653cea905d8925cac09e64d016e3f8d2d0677b6d
beltro39 <sbeltrana@unal.edu.co>
2022-10-31 22:54:23 -0500

    probando

--- pregunta_08.sql
+++ pregunta_08.sql
@@ -41,4 +41,4 @@
 --
 --  >>> Escriba su codigo a partir de este punto <<<
 --
-select avg(c21) from tbl2 group by substr(c23,1,4);
+select substr(c23,1,4), avg(c21) from tbl2 group by substr(c23,1,4);
